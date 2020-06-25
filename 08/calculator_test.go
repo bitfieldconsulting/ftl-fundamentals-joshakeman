5e445dcf3b1e823211dbfdb48cd9891aefa8b01d
Josh Akeman <joshakeman@gmail.com>
2020-06-25 11:37:55 -0400

    completed exercise 8

--- 08/calculator_test.go
+++ 08/calculator_test.go
@@ -13,6 +13,11 @@ func TestAdd(t *testing.T) {
 		{a: 1, b: 1, want: 2},
 		{a: 2, b: 2, want: 4},
 		{a: 6, b: 7, want: 13},
+		{a: -8, b: -3, want: -11},
+		{a: -2, b: 0, want: -2},
+		{a: 0, b: 0, want: 0},
+		{a: 3, b: 5 / 5, want: 4},
+		{a: 3 * 10, b: 6 / 3, want: 32},
 	}
 	for _, testCase := range testCases {
 		got := calculator.Add(testCase.a, testCase.b)
